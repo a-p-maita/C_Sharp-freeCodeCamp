ac337b9e01c40f71a8f67308106d7edb35d474ae
Andreas-PM <andreaspaulmaita.com>
2026-06-05 21:04:11 +0100

    Finished cshapr Basic String Formatting

---
+++ 2026-W24-5__Basic-String-Formatting/Basic-Formatting-Complete-The-Challenge/Basic-Formatting-Complete-The-Challenge.cs
@@ -0,0 +1,25 @@
+﻿// Challenge
+// Print instructions to the end user to let them know where app will output data files
+
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine(
+$@"View English output:
+  c:\Exercise\{projectName}\data.txt"
++ "\n"
+);
+Console.WriteLine(
+$@"{russianMessage}:
+  c:\Exercise\{projectName}\ru-RU\data.txt"
++ "\n"
+);
+
+
+// Cleaner solution
+// I disagree that this matches up the tab/spaces as the actual challenge shows but WHATEVER
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t{englishLocation}\n");
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t{russianLocation}\n");
