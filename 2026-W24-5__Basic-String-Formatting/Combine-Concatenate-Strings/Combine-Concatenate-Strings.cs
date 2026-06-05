b90e0651e3c1f7e19e1543c5240faab15715bf0b
Andreas-PM <andreaspaulmaita.com>
2026-06-05 20:26:51 +0100

    Interpolation

---
+++ 2026-W24-5__Basic-String-Formatting/Combine-Concatenate-Strings/Combine-Concatenate-Strings.cs
@@ -0,0 +1,13 @@
+﻿// This is all quite basic but here
+
+string bookLineOne;
+string bookLineOnePointFive;
+string bookLineTwo = "But then she woke up and realised... it was all a dream";
+string bookLineThree = @"Isn't it crazy that this could have all just ended with a \n";
+
+// This is valid assigning within a WriteLine method that's neat
+Console.WriteLine("\n\n" + (bookLineOne = "Once upon a time there was something or someone or whatever in the world") + "\n" + bookLineTwo + "\n" + bookLineThree + "\n" + bookLineOne + "\n\n");
+
+// Interesting that this is valid and if I don't do the last bookLineOnePointFive it still is but there's a warning
+// That it's assigned but never used
+Console.WriteLine((bookLineOnePointFive = "Once upon a time there WASN'T something or someone or whatever in the world") + "\n" + bookLineTwo + "\n" + bookLineThree);
---
+++ 2026-W24-5__Basic-String-Formatting/Combine-Interpolate-Strings/Combine-Interpolate-Strings.cs
@@ -0,0 +1,27 @@
+﻿// Interpolation is where you have to combine many literal strings
+// and variables into one single formatted message
+
+string batallionNumber = "Charlie";
+string escapeVehicle = "choppa";
+string emergencyCommunication = "Team " + batallionNumber + " get to the " + escapeVehicle;
+
+// Can be more concise with interpolation rather than concat
+string emergencyCommunicationShort = $"Team {batallionNumber} get to the {escapeVehicle}";
+
+
+// Both output the same thing
+Console.WriteLine(emergencyCommunication);
+Console.WriteLine(emergencyCommunicationShort);
+
+
+// Also  useful for multiple data types
+float linuxKernelVersion = 3.21f;
+double numUpdates = 8;
+string updateMessage = $"You have {numUpdates} updates pending to upgrade to kernel version {linuxKernelVersion}";
+Console.WriteLine($"\n\n[CRITICAL] System Warning:\t{updateMessage}\n\n");
+
+
+// Can combine interpolation and verbatim literals
+// Ignore reuse of var. it's just convenient rather than a new float you get it
+Console.WriteLine($@"C:\Programs\FreeCAD {linuxKernelVersion}\Data");
+Console.WriteLine(@$"C:\Programs\FreeCAD {linuxKernelVersion}\Data");
