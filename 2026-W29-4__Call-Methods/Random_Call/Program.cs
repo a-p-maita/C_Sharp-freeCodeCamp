e3f675841e49aa478c85c1a64321b91caba8d0e2
Andreas-PM <andreaspaulmaita@gmail.com>
2026-07-16 09:59:37 +0100

    feat: call stateful Random class and its Next method

---
+++ 2026-W29-4__Call-Methods/Random_Call/Program.cs
@@ -0,0 +1,10 @@
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine($"Dice rolled {roll}");
+int dndRoll = dice.Next(1, 21);
+Console.WriteLine($"DnD D20 dice rolled {dndRoll}");
+// Stateless aka. static methods are ones that don't require current state of app to work
+// Meaning it works without referencing or changing values in memory
+
+// Stateful aka. instance methods are the opposite and do require referencing/changing values in memory
+// They keep track of their state in fields which are variables defined on the class
