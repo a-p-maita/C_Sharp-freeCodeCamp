4e04c1ebe5bc1eebea5cd8e4360f2349eb562049
Andreas-PM <andreaspaulmaita@gmail.com>
2026-07-16 10:31:50 +0100

    feat: complete challenge, use class library to invoke method of a class

---
+++ 2026-W29-4__Call-Methods/Implement_Method_Call_Challenge/Program.cs
@@ -0,0 +1,7 @@
+﻿int firstVal = 500;
+int secondVal = 600;
+int largerVal;
+// largerVal = System.Math.Max(firstVal, secondVal);
+// simplified to:
+largerVal = Math.Max(firstVal, secondVal);
+Console.WriteLine(largerVal);
